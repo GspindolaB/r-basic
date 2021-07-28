9f042c2f36f1b8558a4b573157203f526c289236
unknown <gspindolab>
2021-07-28 12:04:51 -0500

    Trabajando con mis primeros archivos

---
+++ scripts/01Introduccion/03instalaspaquetes.R
@@ -0,0 +1,13 @@
+
+
+#Instala paquetes 
+
+install.packages("tidyverse", dep=TRUE)
+
+#Activa el paquete instalado para trabajar con él
+
+library(tidyverse)
+
+#Desactiva el paquete instalado
+
+detach("package:tidyverse", unload = TRUE)
