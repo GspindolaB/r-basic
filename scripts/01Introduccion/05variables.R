bea4dbe092fe1c4fee840705f884c695272faac9
unknown <gspindolab>
2021-07-28 20:01:07 -0500

    Trabajando con variables

---
+++ scripts/01Introduccion/05variables.R
@@ -0,0 +1,27 @@
+### VARIABLES ###
+
+#Se puede crear una variable asignando el símbolo de =
+
+variable <- 5
+
+#También se puede utilizar el símbolo de < y -, de la siguiente manera <-
+
+variable <- 5
+
+#También se puede utilizar el símbolo de - y >, de la siguiente manera ->
+
+(pi*2)^2 -> variable2
+
+x <- pi^2/2
+
+x
+
+y <- cos(pi/4)
+
+y
+
+redondeado <- round(y, 3)
+
+redondeado
+
+### FUNCIONES ###
